bb6bd033253d23803cff1786b2d9947a05a66a1e
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-12-12 19:59:28 -0600

    trying in rust now

---
+++ main.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+use std::path::Path;
+use clap::{App, load_yaml};
+
+use image_sec::{encrypt_image, decrypt_image};
+
+fn main() -> Result<(), Box<dyn Error>>
+{
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
+
+    let encrypt = matches.value_of("encrypt");
+    match encrypt 
+    {
+        Some(dir) => match encrypt_image(Path::new(dir)) {
+            Ok(_) => println!("Image was successfully encrypted!"),
+            Err(_) => eprintln!("Error encrypting image")
+        },
+        None => ()
+    }
+
+    let decrypt = matches.value_of("decrypt");
+    match decrypt 
+    {
+        Some(dir) => match decrypt_image(Path::new(dir)) {
+            Ok(_) => println!("Image was successfully encrypted!"),
+            Err(_) => eprintln!("Error decrypting image")
+        },
+        None => ()
+    }
+    Ok(())
+}
