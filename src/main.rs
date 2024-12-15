6ef44ed204137de5953da86c127b571833deeb0f
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-12-15 15:55:34 -0600

    UPDATE: added some encryption user input

--- src/main.rs
+++ src/main.rs
@@ -60,12 +60,90 @@ fn main() -> Result<()>
 
         if choice == 1
         {
-            println!("Encrypt an image");
+            // Grab image input from user
+            let mut image_input = String::new();
+            println!("\nPlease input the image name(note must be in current dir): ");
+            io::stdin().read_line(&mut image_input).expect("Failed to read input");
+            image_input = image_input.trim().to_string();
+            let input_path: &str = &image_input;
+            
+            // Grab bin name from user
+            let mut bin_name = String::new();
+            println!("Please name your encrypted .bin file(note add .bin to your fname):");
+            io::stdin().read_line(&mut bin_name).expect("Failed to read input");
+            bin_name = bin_name.trim().to_string();
+            let encrypted_path: &str = &bin_name;
+
+            // Grab hardcoded key
+            
+            
+            // read image into memory
+            let mut input_file = File::open(input_path)?;
+            let mut buffer = Vec::new();
+            input_file.read_to_end(&mut buffer)?;
+            
+            // Hardcoded key (for demonstration)
+            let key: [u8; 32] = 
+            [
+                0x00, 0x01, 0x02, 0x03,
+                0x04, 0x05, 0x06, 0x07,
+                0x08, 0x09, 0x0a, 0x0b,
+                0x0c, 0x0d, 0x0e, 0x0f,
+                0x10, 0x11, 0x12, 0x13,
+                0x14, 0x15, 0x16, 0x17,
+                0x18, 0x19, 0x1a, 0x1b,
+                0x1c, 0x1d, 0x1e, 0x1f
+            ];
+            
+            
+            // Generate IV
+            let mut iv = [0u8; 16];
+            rand::thread_rng().fill(&mut iv);
+            
+            // encrypt the data
+            let encrypted_data = encrypt_data(&buffer, &key, &iv)?;
+            
+            // Write IV + encrypted data to file
+            {
+                let mut out = File::create(encrypted_path)?;
+                out.write_all(&iv)?;
+                out.write_all(&encrypted_data)?;
+            }
+            
+            println!("Encrypted data written to {}", encrypted_path);
+            
+            
             continue;
         }
         else if choice == 2 
         {
             println!("Decrypt an image");
+                
+                
+            let decrypted_path = "decrypted.jpg";
+            let encrypted_path = "encrypted.bin";
+
+            let key: [u8; 32] = 
+            [
+                0x00, 0x01, 0x02, 0x03,
+                0x04, 0x05, 0x06, 0x07,
+                0x08, 0x09, 0x0a, 0x0b,
+                0x0c, 0x0d, 0x0e, 0x0f,
+                0x10, 0x11, 0x12, 0x13,
+                0x14, 0x15, 0x16, 0x17,
+                0x18, 0x19, 0x1a, 0x1b,
+                0x1c, 0x1d, 0x1e, 0x1f
+            ];
+            // Now let's decrypt
+            let decrypted_data = decrypt_file(encrypted_path, &key)?;
+            
+            // write the decrypted data back as an image
+            {
+                let mut out = File::create(decrypted_path)?;
+                out.write_all(&decrypted_data)?;
+            }
+            
+            println!("Decrypted data written to {}", decrypted_path);
             continue;
         }
         else if choice == 3 
@@ -76,57 +154,9 @@ fn main() -> Result<()>
         else 
         {
             println!("invalid choice: {}, please choose again", choice);
+            continue;
         }
-
-        let input_path = "HackerCat.jpg";
-        let encrypted_path = "encrypted.bin";
-        let decrypted_path = "decrypted.jpg";
         
-        // read image into memory
-        let mut input_file = File::open(input_path)?;
-        let mut buffer = Vec::new();
-        input_file.read_to_end(&mut buffer)?;
-    
-        // Hardcoded key (for demonstration)
-        let key: [u8; 32] = 
-        [
-            0x00, 0x01, 0x02, 0x03,
-            0x04, 0x05, 0x06, 0x07,
-            0x08, 0x09, 0x0a, 0x0b,
-            0x0c, 0x0d, 0x0e, 0x0f,
-            0x10, 0x11, 0x12, 0x13,
-            0x14, 0x15, 0x16, 0x17,
-            0x18, 0x19, 0x1a, 0x1b,
-            0x1c, 0x1d, 0x1e, 0x1f
-        ];
-    
-        // Generate IV
-        let mut iv = [0u8; 16];
-        rand::thread_rng().fill(&mut iv);
-    
-        // encrypt the data
-        let encrypted_data = encrypt_data(&buffer, &key, &iv)?;
-    
-        // Write IV + encrypted data to file
-        {
-            let mut out = File::create(encrypted_path)?;
-            out.write_all(&iv)?;
-            out.write_all(&encrypted_data)?;
-        }
-    
-        println!("Encrypted data written to {}", encrypted_path);
-    
-        // Now let's decrypt
-        let decrypted_data = decrypt_file(encrypted_path, &key)?;
-    
-        // write the decrypted data back as an image
-        {
-            let mut out = File::create(decrypted_path)?;
-            out.write_all(&decrypted_data)?;
-        }
-    
-        println!("Decrypted data written to {}", decrypted_path);
-    
         return Ok(());
     }
 
