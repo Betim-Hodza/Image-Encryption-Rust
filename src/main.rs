4956ed7af885d27791b799e0b39bcfe112eeddc1
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2025-01-03 15:26:00 -0600

    UPDATE: encryption is done seamlessly!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::{self, Read, Write};
 use std::process::exit;
+use std::thread;
+use std::time::Duration;
 use anyhow::Result;
 use block_modes::{BlockMode, Cbc};
 use block_modes::block_padding::Pkcs7;
@@ -38,6 +40,14 @@ fn decrypt_file(filepath: &str, key: &[u8; 32]) -> Result<Vec<u8>>
     Ok(decrypted_data)
 }
 
+fn generate_random_key() -> [u8; 32] 
+{
+    let mut rng = rand::thread_rng();
+    let mut key = [0u8; 32];
+    rng.fill(&mut key);
+    key
+}
+
 fn prompt()
 {
     println!("Rust Image-Encryption: AES256");
@@ -63,40 +73,30 @@ fn main() -> Result<()>
             // Grab image input from user
             let mut image_input = String::new();
             println!("\nPlease input the image name(note must be in current dir): ");
-            io::stdin().read_line(&mut image_input).expect("Failed to read input");
+            io::stdin().read_line(&mut image_input).expect("Failed to read image");
+
+            // use that image input to create the path for the image later
             image_input = image_input.trim().to_string();
             let input_path: &str = &image_input;
             
             // Grab bin name from user
             let mut bin_name = String::new();
             println!("Please name your encrypted .bin file(note add .bin to your fname):");
-            io::stdin().read_line(&mut bin_name).expect("Failed to read input");
+            io::stdin().read_line(&mut bin_name).expect("Failed to read bin");
+
+            // similar to the image input but only for the bin path to be created later
             bin_name = bin_name.trim().to_string();
             let encrypted_path: &str = &bin_name;
 
             // Grab hardcoded key
-            
+            let key = generate_random_key();
             
             // read image into memory
             let mut input_file = File::open(input_path)?;
             let mut buffer = Vec::new();
             input_file.read_to_end(&mut buffer)?;
             
-            // Hardcoded key (for demonstration)
-            let key: [u8; 32] = 
-            [
-                0x00, 0x01, 0x02, 0x03,
-                0x04, 0x05, 0x06, 0x07,
-                0x08, 0x09, 0x0a, 0x0b,
-                0x0c, 0x0d, 0x0e, 0x0f,
-                0x10, 0x11, 0x12, 0x13,
-                0x14, 0x15, 0x16, 0x17,
-                0x18, 0x19, 0x1a, 0x1b,
-                0x1c, 0x1d, 0x1e, 0x1f
-            ];
-            
-            
-            // Generate IV
+            // Generate random IV
             let mut iv = [0u8; 16];
             rand::thread_rng().fill(&mut iv);
             
@@ -111,6 +111,11 @@ fn main() -> Result<()>
             }
             
             println!("Encrypted data written to {}", encrypted_path);
+            println!("KEY: (keep this a secret): {:?}", key);
+            println!();
+
+            // sleep for 1 sec
+            thread::sleep(Duration::from_secs(1)); 
             
             
             continue;
